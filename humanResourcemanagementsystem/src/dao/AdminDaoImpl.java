988f9cb2293b2c4b1bf79271e6c703ff87225a81
amitamitvaghamshi9@gmail.comamitgit config --global user.email amitvaghamshi9@gmail.com <amitvaghamshi9@gmail.comgit config --global user.name amit>
2023-01-18 21:54:58 +0530

    added 3

--- humanResourcemanagementsystem/src/dao/AdminDaoImpl.java
+++ humanResourcemanagementsystem/src/dao/AdminDaoImpl.java
@@ -2,23 +2,31 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
+import exception.AdminException;
 import util.DBUtil;
 
 public class AdminDaoImpl implements AdminsDao{
 
 	@Override
-	public boolean loginAdmin(String username,String passworld) {
+	public boolean loginAdmin(String username,String passworld) throws AdminException{
 		boolean get=false;
 		
 		try(Connection conn=DBUtil.getConnection()){
+			PreparedStatement st=conn.prepareStatement("select * from admins where username =? and password = ?");
+			st.setString(1, username);
+			st.setString(2, passworld);
 			
-			PreparedStatement st=conn.prepareStatement("");
-			
-		} catch (Exception e) {
-			// TODO: handle exception
+			ResultSet set=st.executeQuery();
+			if(set.next()) {
+				return true;
+			}else {
+				throw new AdminException("User Not Found!");
+			}
+		}catch (Exception e) {
+			System.err.println(e.getMessage());
 		}
-		
 		return get;
 	}
 
